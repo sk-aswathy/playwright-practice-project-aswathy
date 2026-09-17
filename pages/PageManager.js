9d885a54000f024be2b2d92b4cd16ca8cead1eef
sk-aswathy <aswathysk13@gmail.com>
2026-09-17 08:25:46 +0530

    wa ~qa

--- pages/PageManager.js
+++ pages/PageManager.js
@@ -1,7 +1,7 @@
 import { LoginPage } from "../pages/LoginPage";
 import { ProductPage } from "../pages/ProductPage";
 import { CartPage } from "../pages/CartPage";
-import { CheckoutPage } from "../pages/Checkoutpage.js";
+import { CheckoutPage } from "../pages/CheckoutPage.js";
 import {ReviewOrderPage} from "../pages/ReviewOrderPage"
 import { OrderCreatePage } from "../pages/OrderCreatePage";
 
