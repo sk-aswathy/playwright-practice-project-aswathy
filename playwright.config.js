33fbf4677d4ab1298556a96572c41ad77229db29
sk-aswathy <aswathysk13@gmail.com>
2026-09-18 08:26:43 +0530

    ok

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
   //retries : 1,
 
   use:{
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     video : 'retain-on-failure',
     trace : 'retain-on-failure'
